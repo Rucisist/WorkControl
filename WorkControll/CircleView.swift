6a2e1bd5a77f38775ecc76423f1946f09722b578
Andrew Ilalov <ilalov@wamba.com>
2018-11-21 11:30:03 +0300

    максимально тупое-простое-охуенное приложение

---
+++ WorkControll/CircleView.swift
@@ -0,0 +1,35 @@
+//
+//  CircleView.swift
+//  WorkControll
+//
+//  Created by Андрей Илалов on 20.11.2018.
+//  Copyright © 2018 Андрей Илалов. All rights reserved.
+//
+
+import UIKit
+
+class CircleView: UIView {
+    
+    var path: UIBezierPath?
+    var shapeLayer = CAShapeLayer()
+    
+    override func draw(_ rect: CGRect) {
+        layer.addSublayer(shapeLayer)
+    }
+    
+    func drawAPercentCircle(percent: Double) {
+        let center = CGPoint(x: frame.width/2, y: frame.height/2)
+        let radius = frame.width/2
+
+        path = UIBezierPath(arcCenter: center, radius: radius, startAngle: CGFloat(-Double.pi/2), endAngle: CGFloat(2*Double.pi*percent - 0.5*Double.pi), clockwise: true)
+//        path?.addLine(to: center)
+//        path?.close()
+        
+        shapeLayer.path = path?.cgPath
+        let newColor = UIColor(red: 154.0/255, green: 194.0/255, blue: 197.0/255, alpha: 1.0)
+        shapeLayer.strokeColor = newColor.cgColor
+        let color = tintColor.withAlphaComponent(0.3)
+        shapeLayer.fillColor = UIColor.clear.cgColor
+        shapeLayer.lineWidth = 2.0
+    }
+}
--- WorkControll/ViewController.swift
+++ WorkControll/ViewController.swift
@@ -10,9 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var satrtButton: UIButton!
+    
+    @IBOutlet weak var circleView: CircleView!
+    var timer = Timer()
+    
+    var percent = 0.0
+    var currentDate = Date.timeIntervalSinceReferenceDate
+    var lastDate: Double = UserDefaults.standard.double(forKey: "lastDate")
+    var isContinue: Bool = false
+    
+    static let workingHours = 520.0 * 60.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        if lastDate == 0 {
+            lastDate = Date.timeIntervalSinceReferenceDate
+        }
+        circleView.backgroundColor = view.backgroundColor
+        
+        timer = Timer.scheduledTimer(timeInterval: 0.05, target: self, selector: #selector(doOnTimer), userInfo: nil, repeats: true)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +38,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func startButtonTapped(_ sender: Any) {
+        lastDate = Date.timeIntervalSinceReferenceDate
+        UserDefaults.standard.set(lastDate, forKey: "lastDate")
+    }
+    
+    @objc func doOnTimer() {
+        let timeInterval = Date.timeIntervalSinceReferenceDate - lastDate
+        percent = timeInterval/ViewController.workingHours
+        circleView.drawAPercentCircle(percent: percent)
+        let seconds = round((1-percent) * ViewController.workingHours)
+        let hours = Int(seconds) / 3600
+        let minutes = (Int(seconds) % 3600) / 60
+        
+        
+        let str = minutes/10 >= 1 ? String(format: "%d:%d", hours, minutes) : String(format: "%d:0%d", hours, minutes)
+        
+        satrtButton.setTitle(str, for: .normal)
+    }
+    
 }
 
