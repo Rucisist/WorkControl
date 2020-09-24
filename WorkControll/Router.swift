0eb193ece768e53ad84bfe52b303db919f282096
Rucisist <andreyilse@yandex.ru>
2020-09-25 00:23:20 +0300

    еще раз

--- WorkControll/Router.swift
+++ WorkControll/Router.swift
@@ -7,3 +7,16 @@
 //
 
 import Foundation
+import UIKit
+
+protocol Routable {
+    var from: UIViewController { set get }
+    var to: UIViewController { set get }
+}
+
+
+extension UIViewController {
+    func show(vc: UIViewController) {
+        self.present(vc, animated: true, completion: nil)
+    }
+}
